3281a77b13f12c0ef16be4e908b157499562a46a
bryanlim <blackbeelabs@gmail.com>
2019-12-08 15:31:25 +0800

    added models

--- src/ctpe_project/ctpe_deals_app/models.py
+++ src/ctpe_project/ctpe_deals_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+	name = models.CharField(max_length=128, verbose_name='Name')
+
+class Deal(models.Model):
+	name = models.CharField(max_length=128, verbose_name='Name')
+
+class Topic(models.Model):
+	name = models.CharField(max_length=128, verbose_name='Name')	
